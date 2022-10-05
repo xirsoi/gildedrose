d77f6dc265a9c05b69ec5998689ecc063c31dc87
Adam Chevalier <chevalierAdam2@gmail.com>
2022-10-05 08:14:10 -0500

    changes to .net5 changes formatting to my liking updates starting test to make correct assertions

--- csharpcore/GildedRose/GildedRose.cs
+++ csharpcore/GildedRose/GildedRose.cs
@@ -2,88 +2,88 @@
 
 namespace GildedRoseKata
 {
-    public class GildedRose
+  public class GildedRose
+  {
+    IList<Item> Items;
+    public GildedRose(IList<Item> Items)
     {
-        IList<Item> Items;
-        public GildedRose(IList<Item> Items)
+      this.Items = Items;
+    }
+
+    public void UpdateQuality()
+    {
+      for (var i = 0; i < Items.Count; i++)
+      {
+        if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
         {
-            this.Items = Items;
+          if (Items[i].Quality > 0)
+          {
+            if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+            {
+              Items[i].Quality = Items[i].Quality - 1;
+            }
+          }
+        }
+        else
+        {
+          if (Items[i].Quality < 50)
+          {
+            Items[i].Quality = Items[i].Quality + 1;
+
+            if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+            {
+              if (Items[i].SellIn < 11)
+              {
+                if (Items[i].Quality < 50)
+                {
+                  Items[i].Quality = Items[i].Quality + 1;
+                }
+              }
+
+              if (Items[i].SellIn < 6)
+              {
+                if (Items[i].Quality < 50)
+                {
+                  Items[i].Quality = Items[i].Quality + 1;
+                }
+              }
+            }
+          }
         }
 
-        public void UpdateQuality()
+        if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
         {
-            for (var i = 0; i < Items.Count; i++)
+          Items[i].SellIn = Items[i].SellIn - 1;
+        }
+
+        if (Items[i].SellIn < 0)
+        {
+          if (Items[i].Name != "Aged Brie")
+          {
+            if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
             {
-                if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                {
-                    if (Items[i].Quality > 0)
-                    {
-                        if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                        {
-                            Items[i].Quality = Items[i].Quality - 1;
-                        }
-                    }
-                }
-                else
-                {
-                    if (Items[i].Quality < 50)
-                    {
-                        Items[i].Quality = Items[i].Quality + 1;
-
-                        if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (Items[i].SellIn < 11)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
-
-                            if (Items[i].SellIn < 6)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
-                        }
-                    }
-                }
-
+              if (Items[i].Quality > 0)
+              {
                 if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
                 {
-                    Items[i].SellIn = Items[i].SellIn - 1;
+                  Items[i].Quality = Items[i].Quality - 1;
                 }
-
-                if (Items[i].SellIn < 0)
-                {
-                    if (Items[i].Name != "Aged Brie")
-                    {
-                        if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (Items[i].Quality > 0)
-                            {
-                                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                                {
-                                    Items[i].Quality = Items[i].Quality - 1;
-                                }
-                            }
-                        }
-                        else
-                        {
-                            Items[i].Quality = Items[i].Quality - Items[i].Quality;
-                        }
-                    }
-                    else
-                    {
-                        if (Items[i].Quality < 50)
-                        {
-                            Items[i].Quality = Items[i].Quality + 1;
-                        }
-                    }
-                }
+              }
             }
+            else
+            {
+              Items[i].Quality = Items[i].Quality - Items[i].Quality;
+            }
+          }
+          else
+          {
+            if (Items[i].Quality < 50)
+            {
+              Items[i].Quality = Items[i].Quality + 1;
+            }
+          }
         }
+      }
     }
+  }
 }
--- csharpcore/GildedRoseTests/GildedRoseTest.cs
+++ csharpcore/GildedRoseTests/GildedRoseTest.cs
@@ -4,15 +4,17 @@ using GildedRoseKata;
 
 namespace GildedRoseTests
 {
-    public class GildedRoseTest
+  public class GildedRoseTest
+  {
+    [Fact]
+    public void foo()
     {
-        [Fact]
-        public void foo()
-        {
-            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
-            GildedRose app = new GildedRose(Items);
-            app.UpdateQuality();
-            Assert.Equal("fixme", Items[0].Name);
-        }
+      IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+      GildedRose app = new GildedRose(Items);
+      app.UpdateQuality();
+      Assert.Equal("foo", Items[0].Name);
+      Assert.Equal(0, Items[0].Quality);
+      Assert.Equal(-1, Items[0].SellIn);
     }
+  }
 }
